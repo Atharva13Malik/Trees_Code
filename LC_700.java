a2b18002a4675ef06e69991b44a206be4e670251
Atharva MAlik <atharvasanjaymalik@gmail.com>
2026-06-26 14:15:19 +0530

    Implement searchBST function for binary search tree

---
+++ LC_700.java
@@ -0,0 +1,6 @@
+ if(root==null) return null;
+        if(root.val==val) return root;
+        if(root.val>val) return searchBST(root.left,val);
+        if(root.val<val) return searchBST(root.right,val);
+
+        return root;
